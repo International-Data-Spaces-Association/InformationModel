666c9d93462b6512814723d4417340929e86541e
Christoph Lange <math.semantic.web@gmail.com>
2020-02-11 11:53:57 +0100

    minor documentation fixes

--- utils/rdf_void_annotation.pl
+++ utils/rdf_void_annotation.pl
@@ -26,17 +26,17 @@ my $base_uri = "https://w3id.org/idsa/core/";                               # Pr
 
 # read rdf data
 my $model = RDF::Trine::Model->new();                                       # Generate empty RDF::Trine "model" object. 
-RDF::Trine::Parser->parse_file_into_model($base_uri, $filename, $model );   # Instantiate TTL Parser and parse InfoModel from file.
+RDF::Trine::Parser->parse_file_into_model($base_uri, $filename, $model );   # Instantiate parser and parse InfoModel from file (auto-detects serialization).
 print $model->size . " RDF statements parsed\n";                            # Count and print parsed RDF statements.
 
 # transform 
-my $generator = RDF::Generator::Void->new(inmodel => $model);               # Instantiate Void Generator and read Information Model.
+my $generator = RDF::Generator::Void->new(inmodel => $model);               # Instantiate VoID Generator and read Information Model.
 $generator->urispace($base_uri);                                            # Set URI Space.
-my $voidmodel = $generator->generate;                                       # Generate Void model.
-print $voidmodel->size . " RDF statements parsed\n";                        # Count Void statements.
+my $voidmodel = $generator->generate;                                       # Generate VoID model.
+print $voidmodel->size . " RDF statements parsed\n";                        # Count VoID statements.
 
 # save
-my $serializer = RDF::Trine::Serializer::Turtle->new();                     # Instantiate TTL Serializer.
+my $serializer = RDF::Trine::Serializer::Turtle->new();                     # Instantiate Turtle Serializer.
 open($fh, '>', $void_filename) or die $!;                                   # Open filename to save results.
-$serializer->serialize_model_to_file($fh, $voidmodel);                      # Write Void results to file.
+$serializer->serialize_model_to_file($fh, $voidmodel);                      # Write VoID results to file.
 close($fh);                                                                 # Close file.
